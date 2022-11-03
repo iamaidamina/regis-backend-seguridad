9d0bdf41b496404ecf44d4fac0188b9388d42608
Aida Mina <aida.mina@correounivalle.edu.co>
2022-11-03 16:39:22 -0500

    Setting Sprint Boot Project

---
+++ src/main/java/com/example/regisbackendseguridad/RegisBackendSeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.example.regisbackendseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisBackendSeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisBackendSeguridadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/regisbackendseguridad/RegisBackendSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regisbackendseguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisBackendSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
