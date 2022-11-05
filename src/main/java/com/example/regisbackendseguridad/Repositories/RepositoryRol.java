3589e0789d33c7a6fe9a30e82f5e923d32edcbfd
Aida Mina <iamaidamina@gmail.com>
2022-11-05 03:38:43 -0500

    creating Rol repository

---
+++ src/main/java/com/example/regisbackendseguridad/Repositories/RepositoryRol.java
@@ -0,0 +1,7 @@
+package com.example.regisbackendseguridad.Repositories;
+import com.example.regisbackendseguridad.Models.Rol;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RepositoryRol extends MongoRepository<Rol,String> {
+
+}
