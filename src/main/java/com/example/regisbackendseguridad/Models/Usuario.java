4e573cb5b11d50d6f902f48daf165e91a08f9db8
Aida Mina <iamaidamina@gmail.com>
2022-11-05 01:24:53 -0500

    finish Usuario model

--- src/main/java/com/example/regisbackendseguridad/Models/Usuario.java
+++ src/main/java/com/example/regisbackendseguridad/Models/Usuario.java
@@ -10,4 +10,38 @@ public class Usuario {
     private String seudonimo;
     private String correo;
     private String contrasena;
+
+    public Usuario(String seudonimo, String correo, String contrasena) {
+        this.seudonimo = seudonimo;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+    
+    public String getId(){
+        return id;
+    }
+
+    public String getSeudonimo(){
+        return seudonimo;
+    }
+    
+    public void setSeudonimo(String seudonimo){
+        this.seudonimo = seudonimo;
+    }
+    
+    public String getCorreo(){
+        return correo;
+    }
+    
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+
+    public String getContrasena(){
+        return contrasena;
+    }
+        
+    public void setContrasena(String contrasena){
+        this.contrasena = contrasena;
+    }
 }
