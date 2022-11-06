c0317a83fa3f6e0ec434a39a011f5857390e41f8
Aida Mina <iamaidamina@gmail.com>
2022-11-06 04:12:41 -0500

    creating permisosrol repository

---
+++ src/main/java/com/example/regisbackendseguridad/Repositories/RepositoryPermisosRol.java
@@ -0,0 +1,7 @@
+package com.example.regisbackendseguridad.Repositories;
+import com.example.regisbackendseguridad.Models.PermisosRol;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface  RepositoryPermisosRol extends MongoRepository<PermisosRol,String> {
+    
+}
